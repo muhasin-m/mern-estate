403b46d6a7b67814a32bc99785d4a767a2af57d2
muhasin <mahmudmuhasin@gmail.com>
2024-09-09 12:33:24 +0200

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About MERNEstate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit magni
+        sapiente ea dolores consequuntur, a delectus enim animi commodi
+        reprehenderit laborum labore cupiditate at, iste mollitia modi
+        exercitationem cumque omnis, adipisci veniam sequi. Minus quod inventore
+        amet asperiores, dolorum sit vitae aut rem, ratione omnis placeat
+        distinctio repellat aspernatur architecto.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        voluptate minima delectus, quidem dolorum, sit expedita odit totam, ea
+        recusandae culpa accusamus quisquam assumenda iusto non. Commodi
+        similique iure ullam, consectetur incidunt ratione perspiciatis qui unde
+        atque, quasi, voluptatem dolorum. Illum asperiores ipsum quis sunt
+        perferendis ex fugiat fuga, quidem consequuntur voluptate maiores natus
+        esse. Impedit molestias error assumenda dolores officia natus
+        exercitationem reprehenderit eius ipsa. Illo, a quas exercitationem
+        molestiae et, culpa assumenda voluptas reiciendis at libero temporibus,
+        deserunt cupiditate doloremque non officiis quasi rerum repellendus.
+        Dolores et assumenda cupiditate ratione quis beatae sapiente consectetur
+        earum, illo temporibus. Tempora.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
+        nulla accusantium? Nulla quasi, sequi, veritatis vero beatae ut aliquam
+        architecto recusandae dignissimos sapiente facilis tempore!
+      </p>
+    </div>
+  );
 }
