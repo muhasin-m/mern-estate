3f5a5e43c21583593cfcec1afb31b4588299e529
muhasin <mahmudmuhasin@gmail.com>
2024-09-09 12:46:24 +0200

    update navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -50,7 +50,7 @@ export default function Header() {
         </form>
 
         <ul className="flex gap-4">
-          <Link to="/home">
+          <Link to="/">
             <li className="hidden sm:inline text-slate-700 hover:underline">
               Home
             </li>
